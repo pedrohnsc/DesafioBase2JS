744b52fd1bfdf88b55e45ccdb8b0f3bf2a5c69e4
pedro <pedrohnsc@gmail.com>
2019-09-14 15:20:03 -0300

    Fix properties on basic config

--- tests/createProtractorConfig.js
+++ tests/createProtractorConfig.js
@@ -1,11 +1,28 @@
+const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
+
 module.exports = providedConfig => {
     const defaultConfig = {
-        seleniumAddress: "http://localhost:4444/wd/hub",
         specs: ["../specs/*.spec.js"],
         baseUrl: "http://mantis-prova.base2.com.br",
-
+        directConnect: true,
         onPrepare: () => {
             browser.waitForAngularEnabled(false);
+            jasmine.getEnv().addReporter(
+                new SpecReporter({
+                    suite: {
+                        displayNumber: true
+                    },
+                    spec: {
+                        displayFailed: true,
+                        displayPending: true,
+                        displayDuration: true,
+                        displayStackTrace: true
+                    },
+                    summary: {
+                        displayFailed: true
+                    }
+                })
+            );
         }
     }
     return Object.assign({}, defaultConfig, providedConfig);
