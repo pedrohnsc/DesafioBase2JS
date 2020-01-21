c0b088e4426cac25490a82c201135c72b7ab1beb
pedro <pedrohnsc@gmail.com>
2020-01-20 23:10:48 -0300

    Adiciona parametro para ignorar erro do w3c rodando no selenium grid

--- tests/configurations/chrome.js
+++ tests/configurations/chrome.js
@@ -2,6 +2,7 @@ module.exports.config = require("../createProtractorConfig")({
     capabilities: {
         browserName: "chrome",
         chromeOptions: {
+          'w3c': false,
           args: ["--headless"]
         }
     }
--- tests/createProtractorConfig.js
+++ tests/createProtractorConfig.js
@@ -4,7 +4,7 @@ module.exports = providedConfig => {
     const defaultConfig = {
         specs: ["../specs/*.spec.js"],
         baseUrl: "http://mantis-prova.base2.com.br",
-        directConnect: true,
+        seleniumAddress: "http://localhost:4444/wd/hub",
         onPrepare: () => {
             browser.waitForAngularEnabled(false);
             jasmine.getEnv().addReporter(
